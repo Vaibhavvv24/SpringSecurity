5222b4f1a9aee341adda47c2bb2513a2b02ac67f
Vaibhav <mittalvaibhav277@gmail.com>
2024-03-24 01:24:32 +0530

    added Security config and rolebased auth

---
+++ src/main/java/com/luv2code/springboot/cruddemo/security/SecurityDemo.java
@@ -0,0 +1,52 @@
+package com.luv2code.springboot.cruddemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityDemo {
+
+
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//        UserDetails vaibhav= User.builder().username("vaib").password("{noop}hithere").roles("EMPLOYEE").build();
+//        UserDetails pratham= User.builder().username("prath").password("{noop}hi_there").roles("EMPLOYEE","MANAGER").build();
+//        UserDetails sakshi= User.builder().username("sexy").password("{noop}hi.there").roles("EMPLOYEE","MANAGER","ADMIN").build();
+//
+//        return new InMemoryUserDetailsManager(vaibhav,pratham,sakshi);
+//    }
+    //adding jdbc support so that we dont have to hardcode data of users
+    @Bean
+    public UserDetailsManager userDetails(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer->
+                configurer
+                        .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
+                );
+        //use http basic auth
+        http.httpBasic(Customizer.withDefaults());
+
+        //disable csrf(Cross site request forgery)
+        http.csrf(csrf->csrf.disable());
+
+        return http.build();
+
+    }
+}
